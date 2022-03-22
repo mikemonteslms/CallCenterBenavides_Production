e850d93880a2a1e50a22b6610ef692cf9253394e
rgodinez7612 <81718268+rgodinez7612@users.noreply.github.com>
2022-03-22 16:04:53 -0600

    Se agregan modelos para en un futuro reacomodar submenus en menu Administra promociones

---
+++ Infraestructura/Datos/ModContenedorMnusSubmnus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Datos
+{
+    public class ModContenedorMnusSubmnus
+    {
+        public ModContenedorMnusSubmnus()
+        {
+            this.Submnus = new List<ModSubmnus>();
+        }
+        public List<ModMenus> Menus { get; set; }
+        public List<ModSubmnus> Submnus { get; set; }
+    }
+}
---
+++ Infraestructura/Datos/ModSubmnus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Datos
+{
+    public class ModSubmnus
+    {
+        public int IdMenu { get; set; }
+        public int IdSubMenu { get; set; }
+        public string NombreSubMnu { get; set; }
+        public string URL { get; set; }
+        public string Alt { get; set; }
+        public int Orden { get; set; }
+        public int PadreId { get; set; }
+    }
+}
